961442d35f2feacc99635093f723cd603a110189
Megan Cui <megan.cui.mc@gmail.com>
2018-07-08 19:11:16 -0400

    added button to be done with item

--- src/Todo.js
+++ src/Todo.js
@@ -4,9 +4,10 @@ class Todo extends Component {
   render() {
     return (
       <div>
-        <h1 onClick={() => this.props.deleteTodo(this.props.index)}>
-          {this.props.todo}
-        </h1>
+        <h1>{this.props.todo}</h1>
+        <button onClick={() => this.props.deleteTodo(this.props.index)}>
+          I am done with {this.props.todo}
+        </button>
       </div>
     );
   }
